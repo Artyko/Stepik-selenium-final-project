ac59f775b5ffefad6065c0cd227da2af21c928a4
Artem Kovyryagin <artyko94@gmail.com>
2019-09-15 17:51:11 +0300

    add product page test

--- page_objects/pages/locators.py
+++ page_objects/pages/locators.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
@@ -11,4 +11,14 @@ class LoginPageLocators:
     REGISTER_EMAIL_FORM = (By.CSS_SELECTOR, "input[name='registration-email'].form-control")
     REGISTER_PASSWORD_FORM = (By.CSS_SELECTOR, "input[name='registration-password1'].form-control")
     REGISTER_PASSWORD_CONFIRM_FORM = (By.CSS_SELECTOR, "input[name='registration-password2'].form-control")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    ADDED_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    PRODUCT_COST = (By.CSS_SELECTOR, "div.alert > div > p:nth-child(1) > strong")
+    BASKET_COST = (By.CSS_SELECTOR, "div.alertinner > p > strong")
+
+
 
---
+++ page_objects/test_product_page.py
@@ -0,0 +1,18 @@
+from .pages.product_page import ProductPage
+import pytest
+
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"{link}"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_match_product_name()
+    page.should_match_product_cost()
+
