b0ff62934c3eebd47d0057c798410f2f84a7913a
Artem Kovyryagin <artyko94@gmail.com>
2019-09-15 19:25:04 +0300

    add negative test: element missing check

--- page_objects/pages/product_page.py
+++ page_objects/pages/product_page.py
@@ -16,3 +16,11 @@ class ProductPage(BasePage):
         product_cost = self.browser.find_element(*ProductPageLocators.PRODUCT_COST).text
         basket_cost = self.browser.find_element(*ProductPageLocators.BASKET_COST).text
         assert product_cost == basket_cost, f"Product cost does not match, should {product_cost}, given {basket_cost}"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.ADDED_PRODUCT_NAME), \
+            "Success message is presented, but should not be"
+
+    def success_message_should_be_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.ADDED_PRODUCT_NAME), \
+            "Success message is presented, but should not be"
--- page_objects/test_product_page.py
+++ page_objects/test_product_page.py
@@ -16,3 +16,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_match_product_name()
     page.should_match_product_cost()
 
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = product_base_link
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = product_base_link
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = product_base_link
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.success_message_should_be_disappeared()
