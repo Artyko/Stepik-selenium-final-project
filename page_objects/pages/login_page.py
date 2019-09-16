70711f05e5458337406dcefead0af7521bd14229
Artem Kovyryagin <artyko94@gmail.com>
2019-09-16 20:41:13 +0300

    add basket page tests

--- page_objects/pages/login_page.py
+++ page_objects/pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.browser.current_url, "Login link is not found"
+        assert "login" in self.browser.current_url, "Login link is not found"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_EMAIL_FORM), "Login email form is not presented"
