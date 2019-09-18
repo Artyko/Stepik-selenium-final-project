d0287b103c8df803f5ab34857094a5330dd95422
Artem Kovyryagin <dastet@rambler.ru>
2019-09-18 21:22:36 +0300

    ready for review

--- page_objects/test_product_page.py
+++ page_objects/test_product_page.py
@@ -26,6 +26,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = product_base_link
         page = ProductPage(browser, link)
@@ -35,6 +36,7 @@ class TestUserAddToBasketFromProductPage:
         page.should_match_product_cost()
 
 
+@pytest.mark.need_review
 @pytest.mark.xfail
 @pytest.mark.parametrize('link', urls)
 def test_guest_can_add_product_to_basket(browser, link):
@@ -79,6 +81,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = product_base_link
     page = ProductPage(browser, link)
@@ -86,6 +89,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = product_base_link
     page = ProductPage(browser, link)
